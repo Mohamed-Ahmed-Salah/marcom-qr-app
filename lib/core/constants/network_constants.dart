7380c18d81ed387a86147559d4cfb34d17a4b123
Mohamed Ahmed <90526765+Mohamed-Ahmed-Salah@users.noreply.github.com>
2025-07-23 08:51:00 +0300

    Update AWS api url

--- lib/core/constants/network_constants.dart
+++ lib/core/constants/network_constants.dart
@@ -1,7 +1,7 @@
 abstract class NetworkConstants {
   ///updated from cache singleton
-  static String devUrl = 'http://16.171.141.137:8080';
-  static String releaseUrl = 'http://16.171.141.137:8080';
+  static String devUrl = 'http://16.171.161.240:8080';
+  static String releaseUrl = 'http://16.171.161.240:8080';
 
   static const headers = {'Content-Type': 'application/json; charset=UTF-8'};
   static const pageSize = 10;
